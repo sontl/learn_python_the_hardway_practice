579fd62c8c167982e487b6b37470d12f8e4ed571
sontl <sontl@mac.com>
2016-10-08 23:42:44 +0800

    finish exercise13

---
+++ exercise13.py
@@ -0,0 +1,10 @@
+# Parameters, Unpacking, Variables
+
+from sys import argv
+
+script, first, second, third, nam, sau, bay = argv
+
+print "The script is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "Your thrid variable is:",third
