c7219e4185bf9e44aa780d70d58aa8311eaa218a
sontl <sontl@mac.com>
2016-10-09 12:01:02 +0800

    finish exercise18

---
+++ exercise18.py
@@ -0,0 +1,23 @@
+# Names, Variables, Code ,Functions
+
+# this one is like your scripts with argv
+def print_two(*args):
+    arg1, arg2, arg3 = args
+    print "arg1: %r, arg2: %r" % (arg1, arg2)
+
+# ok, that *args is actually pointless, we can just do this
+def print_two_again(arg1, arg2):
+    print "arg1: %r, arg2: %r" % (arg1, arg2)
+
+# this just takes one argument
+def print_one(arg1):
+    print "arg1: %r" % arg1
+
+# this one takes no argument
+def print_none():
+    print "I got nothin'."
+
+print_two("Lam", "Son", "Three")
+print_two_again("Minh", "Hue")
+print_one("Lam Son")
+print_none()
